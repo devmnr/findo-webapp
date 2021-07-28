415b5e97e61b8b53a6166e343e96ea77991fbcc6
devmnr <87806903+devmnr@users.noreply.github.com>
2021-07-28 21:59:05 +0530

    Update HomeController.java

--- src/main/java/com/springboot/fundraising/controller/HomeController.java
+++ src/main/java/com/springboot/fundraising/controller/HomeController.java
@@ -2,17 +2,24 @@ package com.springboot.fundraising.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.fundraising.repository.Donor;
 
-@Controller
-@RequestMapping("/")
+@RestController
 public class HomeController {
 	
-	@RequestMapping("/home")
+//	@RequestMapping("/home")
+//	public String home() {
+//		
+//		return "home";
+//	}
+	
+	@GetMapping("/home")
 	public String home() {
 		
 		return "home";
